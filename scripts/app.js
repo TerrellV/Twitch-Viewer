5965adff2739a28c99fb26c06aad1ebe91e37e6d
Terrell V (MirPresT) <terrell.vest@gmail.com>
2015-09-05 13:04:35 -0700

    established a custom directive to use on the cards. This directive allows for specific dom manipulation and isolate scope

--- scripts/app.js
+++ scripts/app.js
@@ -1,17 +1,26 @@
 (function() {
     angular.module('myApp', ["ngAnimate"])
-        .controller('cardsCtrl', cardsCtrl);
+        .controller('cardsCtrl', cardsCtrl)
+        .directive('myDir', ['$timeout',dirSample]);
 
     function cardsCtrl($scope) {
 
         var vm = this;
-        var b = $("#btn");
-        var icon = $("#icon");
-        vm.animateButton = function startButtonAnimation() {
+        vm.channels = [
+            {"name": "AppRobotPerson"},
+            {"name": "ChinaDino"},
+            {"name": "Degroso"},
+            {"name": "Bollina"},
+            {"name": "Targareous"},
+            {"name": "Randopa"}
+        ];
+
+        vm.animateButton = function(index) {
+            // console.log(vm.channels[index].name);
             animate();
-            var id = window.setTimeout(showCardInfo, 800);
+            var id = window.setTimeout(vm.showCardInfo, 800);
         };
-        vm.animateButtonBack = function animateBack() {
+        vm.animateButtonBack = function() {
             b.removeClass("animate-fill");
             b.addClass("animate-fill-backwards");
             icon.removeClass("animate-hide");
@@ -27,9 +36,34 @@
             b.removeClass("animate-fill-backwards");
             icon.removeClass("animate-show");
         }
-        function showCardInfo() {
-            vm.showCardInfo = true;
-            $scope.$apply();
-        }
     }
+
+    // custom directive to keep track of dom elements of individual cards...
+        function dirSample($timeout) {
+            return {
+                templateUrl: '/partials/cardContent.html',
+                scope: {
+                    channel: '=',
+                    appear: '='
+                },
+                link: function(scope, element, attributes){
+                    var streamContent = element.find('#info-stream'),
+                        frontButton = element.find('#btn'),
+                        personIcon = frontButton.children(),
+                        exitButton = element.find('#info-close-btn');
+                    // animate front button to fill and then fade out
+                    frontButton.bind("click", function(){
+                        $(this).addClass("animate-fill");
+                        personIcon.fadeOut(800);
+                        $timeout(showInfo,800);
+                        function showInfo(){
+                            console.log('viola');
+                            console.log( scope.appear = true);
+                            frontButton.fadeOut(200);
+                        }
+                    });
+                }
+            }
+        }
+
 })()

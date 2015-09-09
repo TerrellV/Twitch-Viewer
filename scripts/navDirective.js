b9e95a2b008d1a21b2a6a7c97284462a9f0dabb6
Terrell V <terrell.vest@gmail.com>
2015-09-09 12:54:57 -0700

    add style changes to nav bar depending on which is active

--- scripts/navDirective.js
+++ scripts/navDirective.js
@@ -4,7 +4,34 @@
 
         function navDir() {
             return {
-                templateUrl: '/partials/nav.html'
+                templateUrl: '/partials/nav.html',
+
+                link: function(scope,element,attr) {
+
+                  var tabAll = element.find('#tab-all'),
+                      tabOnline = element.find("#tab-online"),
+                      tabOffline = element.find("#tab-offline");
+
+                  init('all'); //starting tab to show
+
+                  function init(str){
+                    scope.activeTab = str;
+                  }
+
+                  tabAll.bind('click', function(){
+                    scope.activeTab = 'all';
+                    scope.$apply();
+                  });
+                  tabOnline.bind('click', function(){
+                    scope.activeTab = 'online';
+                    scope.$apply();
+                  });
+                  tabOffline.bind('click', function(){
+                    scope.activeTab = 'offline';
+                    scope.$apply();
+                  });
+
+                }
             }
         }
 

083e9c413ec14a1bdf49d34c2cbcc7f6baf6f3d9
Terrell V (MirPresT) <terrell.vest@gmail.com>
2015-09-14 19:24:18 -0700

    delete accidental duplicates

--- build/app.com.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-
-(function () {
-    angular.module('myApp', ["ngAnimate", "factories"]);
-})();
-//# sourceMappingURL=app.com.js.map
--- build/app0.es6
+++
@@ -1,6 +0,0 @@
-"use strict";
-
-(function () {
-    angular.module('myApp', ["ngAnimate", "factories"]);
-})();
-//# sourceMappingURL=app0.es6.map
